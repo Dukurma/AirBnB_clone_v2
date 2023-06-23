f214d1bc4a4b0ef4e630374b5641d89f24ade8a9
Dukurma <barryyabilsu@gmail.com>
2023-06-23 03:43:18 -0700

    test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -21,8 +21,8 @@ class test_fileStorage(unittest.TestCase):
         """ Remove storage file at end of tests """
         try:
             os.remove('file.json')
-        except:
-            pass
+        except OSError as oserr:
+            print(oserr)
 
     def test_obj_list_empty(self):
         """ __objects is initially empty """
